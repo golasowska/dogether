ad6373ea9876a4cc0d906867295c759ea9555f75
Anna Golasowska <annagolasowska@gmail.com>
2018-01-08 18:28:54 +0100

    added Firebase and Redux

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import Firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDjmyqfb-Olrz8xpTzK6B5Ry_x29Ut7dW4",
+    authDomain: "dogether-d802f.firebaseapp.com",
+    databaseURL: "https://dogether-d802f.firebaseio.com",
+    projectId: "dogether-d802f",
+    storageBucket: "",
+    messagingSenderId: "375326882369"
+  };
+
+  Firebase.initializeApp(config);
