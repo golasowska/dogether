b6513770084de44af08f19137c63eaa94c06bfcc
Anna Golasowska <annagolasowska@gmail.com>
2018-01-31 18:26:50 +0100

    added responsive navigation

--- src/containers/Navigation.js
+++ src/containers/Navigation.js
@@ -10,50 +10,61 @@ class Navigation extends React.Component {
 
   render() {
     return (
-      <nav className="navbar navbar-default">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
         <div className="container-fluid">
-          <div className="navbar-header">
-            <Link to="/home" className="navbar-brand text-danger">
-              dogether
-            </Link>
-          </div>
+          <Link to="/home" className="navbar-brand text-danger">
+            dogether
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
 
-          <ul id="navig" className="nav navbar-nav navbar-right">
-            <li className="nav-item" key={1}>
-              <Link className="nav-link active text-danger" to="/gallery">
-                Gallery
-              </Link>
-            </li>
-            <li className="nav-item" key={2}>
-              <Link className="nav-link text-danger" to="/dogfriendly">
-                Dog friendly
-              </Link>
-            </li>
-            <li className="nav-item" key={3}>
-              <Link className="nav-link text-danger" to="/vet">
-                Vet
-              </Link>
-            </li>
-            <li className="nav-item" key={4}>
-              <Link className="nav-link text-danger" to="/adoption">
-                Adoption
-              </Link>
-            </li>
-            <li className="nav-item" key={5}>
-              <Link className="nav-link text-danger" to="/myaccount">
-                My Account
-              </Link>
-            </li>
-            <li className="nav-item" key={6}>
-              <a
-                id="signout"
-                className="nav-link text-danger"
-                onClick={() => this.handleSignout()}
-              >
-                Sign out
-              </a>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="nav navbar-nav navbar-right">
+              <li className="nav-item" key={1}>
+                <Link className="nav-link active text-danger" to="/gallery">
+                  Gallery
+                </Link>
+              </li>
+              <li className="nav-item" key={2}>
+                <Link className="nav-link text-danger" to="/dogfriendly">
+                  Dog friendly
+                </Link>
+              </li>
+              <li className="nav-item" key={3}>
+                <Link className="nav-link text-danger" to="/vet">
+                  Vet
+                </Link>
+              </li>
+              <li className="nav-item" key={4}>
+                <Link className="nav-link text-danger" to="/adoption">
+                  Adoption
+                </Link>
+              </li>
+              <li className="nav-item" key={5}>
+                <Link className="nav-link text-danger" to="/myaccount">
+                  My Account
+                </Link>
+              </li>
+              <li className="nav-item" key={6}>
+                <a
+                  id="signout"
+                  className="nav-link text-danger"
+                  onClick={() => this.handleSignout()}
+                >
+                  Sign out
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
